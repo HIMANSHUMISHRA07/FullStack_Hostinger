f84a51846973612306782aa158e8c81935052ffb
HIMANSHUMISHRA07 <himanshumishra12527@gmail.com>
2025-11-10 08:13:42 +0530

    update env and config file

--- hrms-frontend-master/src/config.js
+++ hrms-frontend-master/src/config.js
@@ -1,4 +1,6 @@
 // src/config.js
-const BASE_URL = "http://localhost:8080";
+///////////const BASE_URL = "http://localhost:8080";
 
+//const BASE_URL = "https://fullstack-hostinger-7.onrender.com/api";
+const BASE_URL = process.env.REACT_APP_API_URL;
 export default BASE_URL;
